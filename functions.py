7b356cf9ad088c4580ca6ee3526684f81c8a71de
Ali Öktem Ediz <alioktemediz@192.168.88.48>
2023-10-30 17:23:36 +0300

    find_and_get_text,dataset compenets added

--- functions.py
+++ functions.py
@@ -11,23 +11,16 @@ import chromedriver_autoinstaller
 import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-import chromedriver_autoinstall
-from master_functions import *
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
 #############Selenium Compenents (application)###################
-
-
 def open_chrome_browser():
-
-    driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install()))
-
-    print("Chrome tarayıcı açıldıı.")
-    return driver
-
-
+    try:
+        chromedriver_autoinstaller.install()
+        driver = webdriver.Chrome()
+        print("Chrome tarayıcı açıldıı.")
+        return driver
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
 
 def navigate_to_url(driver, url):
     try:
@@ -36,14 +29,12 @@ def navigate_to_url(driver, url):
     except Exception as e:
         print(f"Hata oluştu: {e}")
 
-
 def close_browser(driver):
     try:
         driver.quit()
         print("Tarayıcı kapatıldı.")
     except Exception as e:
         print(f"Hata oluştu: {e}")
-
 
 def find_element_by_xpath(driver, xpath):
     try:
@@ -53,7 +44,6 @@ def find_element_by_xpath(driver, xpath):
         print(f"Element bulunamadı: {xpath}")
         return None
 
-
 def find_elements_by_xpath(driver, xpath):
     try:
         elements = driver.find_elements(By.XPATH, xpath)
@@ -61,7 +51,6 @@ def find_elements_by_xpath(driver, xpath):
     except NoSuchElementException:
         print(f"Elementler bulunamadı: {xpath}")
         return []
-
 
 def give_time(sleep_time):
     try:
@@ -72,50 +61,59 @@ def give_time(sleep_time):
         print(f"Hata oluştu: {e}")
 
 
-def find_and_set_text(driver, xpath_field, text):
+def find_and_set_text(driver, xpath, text):
     try:
-        element = find_element_by_xpath(driver, xpath_field)
+        element = find_element_by_xpath(driver, xpath)
         element.send_keys(text)
-        print(f"'{text}' metni belirtilen alana yazıldı: {xpath_field}")
+        print(f"'{text}' metni belirtilen alana yazıldı: {xpath}")
     except NoSuchElementException:
-        print(f"Element bulunamadı: {xpath_field}")
+        print(f"Element bulunamadı: {xpath}")
     except Exception as e:
         print(f"Hata oluştu: {e}")
 
-
-def find_and_click_element(driver, xpath_field):
+def find_and_click_element(driver, xpath):
     try:
-        element = find_element_by_xpath(driver, xpath_field)
+        element = find_element_by_xpath(driver, xpath)
         element.click()
-        print(f"Elemente tıklandı: {xpath_field}")
+        print(f"Elemente tıklandı: {xpath}")
     except NoSuchElementException:
-        print(f"Element bulunamadı: {xpath_field}")
+        print(f"Element bulunamadı: {xpath}")
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+
+def find_and_get_text(driver,xpath):
+    try:
+        element = find_element_by_xpath(driver,xpath)
+        text_element = element.text
+        return text_element
+        print(f"Element Text alındı: {xpath}")
+    except NoSuchElementException:
+        print(f"Element bulunamadı: {xpath}")
     except Exception as e:
         print(f"Hata oluştu: {e}")
 
 
-#  def click_element(element):
-#     if element:
-#         try:
-#             element.click()
-#             print("Elemente tıklandı.")
-#         except Exception as e:
-#             print(f"Hata oluştu: {e}")
-#     else:
-#         print("Element bulunamadı.")
-
-# def set_text(element, text):
-#     if element:
-#         try:
-#             element.send_keys(text)
-#             print(f"'{text}' metni belirtilen alana yazıldı.")
-#         except Exception as e:
-#             print(f"Hata oluştu: {e}")
-#     else:
-#         print("Element bulunamadı.")
 
 #############Selenium Compenents (application)###################
 
+#######Dataset Compenents########
+
+def write_dataset_to_excel(excel_path,dataframe):
+    dataframe.to_excel(excel_path, index=False, engine='openpyxl')
+
+def read_excel_to_dataset(excel_path):
+    return pd.read_excel(excel_path, engine='openpyxl')
+
+def create_empty_dataset():
+    return pd.DataFrame()
+
+def add_column(dataframe, column_name):
+    dataframe[column_name] = None
+    return dataframe
+
+
+
+#######Dataset Compenents########
 
 def get_request(method, url, data, headers):
     return rq.request(method=method, url=url, data=data, headers=headers).json()
@@ -125,14 +123,10 @@ def yaz(text):
     return print(text)
 
 
-def read_excel(excel_path):
-    return pd.read_excel(excel_path, engine='openpyxl')
-
-
 def get_row(df, row):
     return df.iloc[row]
 
-
+#######Ansible runner########
 def run_ansible_playbook(script, os, host_ip, username, password):
 
     my_inventory = {"windows": {
@@ -192,6 +186,7 @@ def run_ansible_playbook(script, os, host_ip, username, password):
 
     r = ansible_runner.run(playbook=playbook, inventory=my_inventory[os], envvars={
                            "ANSIBLE_SHOW_CUSTOM_STATS": True, "ANSIBLE_STDOUT_CALLBACK": "json"})
+    
 
     string_encode = r.stdout.read().encode("ascii", "ignore")
     string_decode = string_encode.decode()
@@ -204,7 +199,7 @@ def run_ansible_playbook(script, os, host_ip, username, password):
     try:
 
         output = r.stdout.read()
-        # embed()
+        #embed()
         if "error" not in output.lower():
 
             indexOfOutPut = output.find("{", 0)
@@ -213,8 +208,7 @@ def run_ansible_playbook(script, os, host_ip, username, password):
             key0 = list(jsonOutput['stats'].keys())[0]
 
             if jsonOutput['stats'][key0]['failures'] > 0 or jsonOutput['stats'][key0]['unreachable'] > 0:
-                msg = ",".join([",".join([k['hosts'][key0]['msg']
-                               for k in z['tasks']]) for z in json.loads(res)['plays']])
+                msg = ",".join([",".join([k['hosts'][key0]['msg']for k in z['tasks']]) for z in json.loads(res)['plays']])
                 raise Exception(msg)
             else:
                 return "SUCCESS"
@@ -224,5 +218,30 @@ def run_ansible_playbook(script, os, host_ip, username, password):
         print(e)
         raise Exception(e)
 
+#######Ansible runner########
 
-# close_browser(driver_test)
+
+
+# give_time(5)
+# hotel_data=[]
+# driver_test=open_chrome_browser()
+# navigate_to_url(driver_test,"https://www.trivago.com.tr/tr/srl/otel-adana-t%C3%BCrkiye?search=200-15236;dr-20231106-20231107;pr-3525-2147483647")
+# give_time(3)
+
+# xpath_names= "//li[@data-testid='accommodation-list-element']//button[@data-testid='item-name']//span[@itemprop='name']"
+# xpath_prices = "//li[@data-testid='accommodation-list-element']//p[@data-testid='recommended-price']"
+
+# hotel_names_elements=find_elements_by_xpath(driver_test,xpath_names)
+# hotel_prices_elements = find_elements_by_xpath(driver_test, xpath_prices)
+
+# for name_element, price_element in zip(hotel_names_elements, hotel_prices_elements):
+#     hotel_name = name_element.text
+#     hotel_price = price_element.text
+#     hotel_data.append([hotel_name, hotel_price])
+
+# df = pd.DataFrame(hotel_data, columns=["Otel Adı", "Fiyat"])
+# print(df)
+
+# write_dataset_to_excel("/Users/alioktemediz/Desktop/robeniceAgent/hotel.xlsx",df)
+
+#close_browser(driver_test)
